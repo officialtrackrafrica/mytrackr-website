9a1f672f025aabd913225a24a631d9a9cad0ec02
Fisayomii <fisayomiaiyetogbon@gmail.com>
2026-05-21 16:32:23 +0100

    more ui fixes

--- src/components/MyTrackrSections/DetailedFeatures.tsx
+++ src/components/MyTrackrSections/DetailedFeatures.tsx
@@ -9,10 +9,10 @@ const DetailedFeatures: React.FC = () => {
     <section className="py-24 space-y-24" id='features'>
       <div className="text-center mb-16 px-6">
         <h2 className="text-3xl md:text-5xl font-bold text-brand-dark mb-4 md:leading-14">
-          Simple financial management tool for solo <br className="hidden md:block" />  entrepreneurs and growing Businesses
+          Simple financial management tool for solo <br className="hidden md:block" />  entrepreneurs and growing businesses
         </h2>
         <p className="text-[#475467] max-w-2xl mx-auto text-xl">
-          Whether you’re a vendor, a school principal, or a founder launching their first business, MyTrackr is built to give you insight into your business numbers, no complicated system involved
+          Whether you’re a vendor, a school principal, or a founder launching their first business, MyTrackr is built to give you insight into your business numbers, no complicated system involved.
         </p>
       </div>
 
